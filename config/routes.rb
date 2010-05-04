539df606c090144de200518b61ab14b26be7fe68
Kieran Pilkington <kieran@katipo.co.nz>
2010-05-04 15:28:52 +1200

    Use config/translatable.yml in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
+require 'kete_translatable_content'
+
 # extend routes for translations of kete content
 ActionController::Routing::Routes.draw do |map|
   # translations routes
   # note that we don't want to override the standard routes handled below
   # restful index is the same as our normal index action, so we limit it to that
-  map.resources :baskets, :path_prefix => ':urlified_name', :only => [:index], :has_many => :translations
-
+  TRANSLATABLES.each do |name, spec_hash|
+    map.resources name.camelize.constantize.table_name.to_sym, :path_prefix => ':urlified_name', :only => [:index], :has_many => :translations
+  end
 end
