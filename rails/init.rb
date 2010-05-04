ef33c4fb9cf822d70a7de00a227242289203b640
Kieran Pilkington <kieran@katipo.co.nz>
2010-05-04 16:33:20 +1200

    Further simplify the install process, and squash the development reload bug in the process

--- rails/init.rb
+++ rails/init.rb
@@ -2,7 +2,25 @@ require 'mongo_translatable'
 require 'kete_translatable_content'
 require 'kete_translatable_content_helper'
 
-ActionController::Base.send(:helper, KeteTranslatableContentHelper)
+config.to_prepare do
+  if IS_CONFIGURED
+    TRANSLATABLES.each do |name, spec_hash|
+      name.camelize.constantize.send(:mongo_translate, spec_hash['translatable_attributes'])
+    end
+
+    ApplicationController.class_eval do
+      before_filter :reload_standard_baskets
+      helper KeteTranslatableContentHelper
+      def reload_standard_baskets
+        Rails.logger.info "[Kete Translatable Content]: Reloading Standard Baskets to be locale aware"
+        @site_basket.reload
+        @help_basket.reload
+        @about_basket.reload
+        @documentation_basket.reload
+      end
+    end
+  end
+end
 
 # precedence over a plugin or gem's (i.e. an engine's) app/views
 # this is the way to go in most cases,
