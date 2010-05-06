ef0f8d4b2f9260f5e7e56a089d2c05de986b63ec
Kieran Pilkington <kieran@katipo.co.nz>
2010-05-06 12:04:39 +1200

    Update test so remove references to old files

--- generators/kete_translatable_content_tests/templates/configure_kete_content_translation_test.rb
+++ generators/kete_translatable_content_tests/templates/configure_kete_content_translation_test.rb
@@ -1,48 +1,42 @@
 require 'test_helper'
 
-# have to do this here because normally it is done after site configuration
-# in environment.rb
-require 'configure_kete_content_translation'
-
-include ConfigureKeteContentTranslation
-
-extend_with_translatable_attributes(Basket, :name)
-
+require 'mongo_translatable'
+require 'kete_translatable_content'
+Basket.send(:mongo_translate, :name)
 
 class ConfigureKeteContentTranslationTest < ActiveSupport::TestCase
   # test that models are translatable by making sure they respond
-  # to mongo_translatable instance methods 
+  # to mongo_translatable instance methods
   context "A basket" do
-    setup do 
+    setup do
       @basket = Basket.first
     end
-    should "respond to locale" do 
+    should "respond to locale" do
       assert @basket.respond_to?(:locale)
     end
 
-    should "respond to translations" do 
+    should "respond to translations" do
       assert @basket.respond_to?(:translations)
     end
 
-    should "respond to translations_locales" do 
+    should "respond to translations_locales" do
       assert @basket.respond_to?(:translations_locales)
     end
 
-    should "respond to translate" do 
+    should "respond to translate" do
       assert @basket.respond_to?(:translate)
     end
 
-    should "respond to translation_for" do 
+    should "respond to translation_for" do
       assert @basket.respond_to?(:translation_for)
     end
 
-    should "respond to available_in_these_locales" do 
+    should "respond to available_in_these_locales" do
       assert @basket.respond_to?(:available_in_these_locales)
     end
 
-    should "respond to needed_in_these_locales" do 
+    should "respond to needed_in_these_locales" do
       assert @basket.respond_to?(:needed_in_these_locales)
     end
-
   end
 end
