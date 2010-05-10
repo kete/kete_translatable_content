927ec300a7490519d2bbce50e9d76fb0e79d0b09
Walter McGinnis <walter@katipo.co.nz>
2010-05-10 16:23:15 +1200

    moved this one method directly into init

--- lib/kete_translatable_content_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'kete_translatable_content'
-
-module KeteTranslatableContentHelper
-  def kete_translatable_content?
-    translatable_controllers = TRANSLATABLES.keys.collect(&:pluralize)
-    translatable_controllers.include?(params[:controller]) &&
-      TRANSLATABLES[params[:controller].singularize]['views'].include?(params[:action])
-  end
-end
