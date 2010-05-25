e002fd2eb14715782f216ba4d33a41f50327cde9
Kieran Pilkington <kieran@katipo.co.nz>
2010-05-25 12:39:50 +1200

    Explanation of what is happening in the tag overides

--- lib/kete_translatable_content/extensions/models/tag.rb
+++ lib/kete_translatable_content/extensions/models/tag.rb
@@ -1,6 +1,23 @@
 # extensions to the kete tag model
 class Tag
+  # When creating a tag, check both the tags model, and tag translations
+  # This is done so that for each tag we don't end up with additiona Tag
+  # objects, but rather, the original tag object with multiple translations
+  #
+  # Example Usage:
+  #
+  # >> I18n.locale = :en
+  # >> t = Tag.find_or_create_with_like_by_name('Welcome')
+  # => #<Tag id: 1, name: "Welcome", locale: "en", original_locale: "en">
+  # >> t.translate(:name => 'Accueil', :locale => :fr)
+  # => #<Tag::Translation name: "Accueil", tag_id: 1, _id: ObjectID('4bfb1a87f42ce12372000002'), locale: "fr", translatable_locale: "en">
+  #
+  # >> I18n.locale = :fr
+  # >> Tag.find_or_create_with_like_by_name('Accueil')
+  # => #<Tag id: 1, name: "Accueil", locale: "fr", original_locale: "en">
+
   # redefine so if no tag is found initially, we look for translations too
+  # this method is used by acts-as-taggable-on for all creations of tags
   def self.find_or_create_with_like_by_name(name)
     # first, see if any tags exist with this name
     existing_tag = first(:conditions => ["name LIKE ?", name])
