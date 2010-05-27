55420b6dec1e63bc103e6c799d1716eb0f306b58
Kieran Pilkington <kieran@katipo.co.nz>
2010-05-28 10:45:26 +1200

    Getting the display of additional footer content working properly

--- lib/kete_translatable_content/extensions/models/basket.rb
+++ lib/kete_translatable_content/extensions/models/basket.rb
@@ -12,4 +12,23 @@ class Basket
     end
     basket_list
   end
+
+  def additional_footer_content_with_inheritance
+    value = parsed_value_from(self._configurable_settings.find_by_name('additional_footer_content'))
+    value ||= parsed_value_from(self.site_basket._configurable_settings.find_by_name('additional_footer_content'))
+    value
+  end
+
+  private
+
+  def parsed_value_from(setting)
+    if setting
+      # Original translation values will come back as YAML, but translations are just strings
+      value = (setting.original_locale == setting.locale) ? YAML.load(setting.value) : setting.value
+      # If the value is blank, return nil, else return the value
+      value.to_s.squish.present? ? value : nil
+    else
+      nil
+    end
+  end
 end
