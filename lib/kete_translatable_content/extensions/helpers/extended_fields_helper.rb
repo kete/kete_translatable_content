654ef279cb8a624a750de1cc0a843ad0675a8d16
Kieran Pilkington <kieran@katipo.co.nz>
2010-05-28 11:46:22 +1200

    Get extended field label and example working on the extended field show page

--- lib/kete_translatable_content/extensions/helpers/extended_fields_helper.rb
+++ lib/kete_translatable_content/extensions/helpers/extended_fields_helper.rb
@@ -1,8 +1,17 @@
 # we only override specific extended field uses for translation
 # rather than overriding find
 ExtendedFieldsHelper.module_eval do
-  def extended_field_example(extended_field)
+  def label_show_column(extended_field)
+    translated_label = extended_field.label_translation_for(I18n.locale) || extended_field.label
+    h(translated_label)
+  end
+
+  def example_show_column(extended_field)
     translated_example = extended_field.example_translation_for(I18n.locale) || extended_field.example
     h(translated_example)
+  end
+
+  def extended_field_example(extended_field)
+    example_show_column(extended_field)
   end
 end
