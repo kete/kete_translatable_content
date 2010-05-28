2f1b8af2e24921298f6e4863ac5864039871373e
Kieran Pilkington <kieran@katipo.co.nz>
2010-05-28 12:08:27 +1200

    Only try to redefine the public_tags/private_tags if they are already set, else db:migrate fails

--- lib/kete_translatable_content/extensions/tagging_overides.rb
+++ lib/kete_translatable_content/extensions/tagging_overides.rb
@@ -1,16 +1,24 @@
+# Redefines public_tags and private_tags to translate the returned tag objects
+# Make sure these are only done when the class they are being included into already responds
+# to public_tags, otherwise it'll end up trying to alias a method that doesn't exist
+
 module TaggingOverides
   unless included_modules.include? TaggingOverides
     def self.included(klass)
       klass.class_eval do
 
-        alias_method(:public_tags_orig, :public_tags) unless self.new.respond_to?(:public_tags_orig)
-        def public_tags
-          Tag.swap_in_translation_for_multiple(public_tags_orig)
+        if self.new.respond_to?(:public_tags)
+          alias_method(:public_tags_orig, :public_tags) unless self.new.respond_to?(:public_tags_orig)
+          def public_tags
+            Tag.swap_in_translation_for_multiple(public_tags_orig)
+          end
         end
 
-        alias_method(:private_tags_orig, :private_tags) unless self.new.respond_to?(:private_tags_orig)
-        def private_tags
-          Tag.swap_in_translation_for_multiple(private_tags_orig)
+        if self.new.respond_to?(:private_tags)
+          alias_method(:private_tags_orig, :private_tags) unless self.new.respond_to?(:private_tags_orig)
+          def private_tags
+            Tag.swap_in_translation_for_multiple(private_tags_orig)
+          end
         end
 
       end
