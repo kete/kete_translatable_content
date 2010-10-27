64e55ed911716892efc88c01bb02dca903def209
Breccan <jalnyfen@gmail.com>
2010-10-28 10:33:24 +1300

    Extended translation helper to pull out extended fields

--- lib/kete_translatable_content/extensions/helpers/translations_helper.rb
+++ lib/kete_translatable_content/extensions/helpers/translations_helper.rb
@@ -6,4 +6,12 @@ TranslationsHelper.module_eval do
       value
     end
   end
+
+  def localized_label_for(attribute_key)
+    if @original.extended_content_values.keys.include?(attribute_key.to_s)
+      return @original.send(attribute_key)
+    else
+      t(@translatable_class.name.tableize + '.' + 'form' + '.' + attribute_key.to_s)
+    end
+  end
 end
