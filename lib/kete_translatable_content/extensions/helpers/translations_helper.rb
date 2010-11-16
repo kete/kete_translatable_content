302544d866882ddb673637e968dd53d62c92233d
Breccan <jalnyfen@gmail.com>
2010-11-17 04:25:09 +1300

    Updated translations helpers for choice hashes

--- lib/kete_translatable_content/extensions/helpers/translations_helper.rb
+++ lib/kete_translatable_content/extensions/helpers/translations_helper.rb
@@ -16,4 +16,29 @@ TranslationsHelper.module_eval do
       t(@translatable_class.name.tableize + '.' + 'form' + '.' + attribute_key.to_s)
     end
   end
+
+    %w{untranslated translated}.each do |term|
+    define_method(term + '_values_with_localized_labels') do
+      set_original
+      raise "No object supplied to translate." if @original.blank?
+      raise "No matching translation available." if term == 'translated' && @translation.blank?
+
+      @original.translatable_attributes.collect do |attribute_key|
+        attribute_key = check_for_value_hash(attribute_key)
+        value = (term == 'untranslated') ? @original.send(attribute_key) : @translation[attribute_key]
+        { :attribute_key => attribute_key,
+          :localized_label => localized_label_for(attribute_key),
+          :value => modify_translation_value(value, @original) }
+      end
+    end
+  end
+
+  def check_for_value_hash(value)
+    if  value.is_a?(Hash) && value.keys.include?('value') && value.keys.include?('label')
+      value["label"]
+    else
+      value
+    end
+  end
+
 end
