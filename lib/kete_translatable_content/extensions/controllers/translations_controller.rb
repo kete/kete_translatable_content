5b2f8fd40f2b070a584e11650c3cb82593cee015
Breccan <jalnyfen@gmail.com>
2010-11-23 17:37:07 +1300

    Translations pull out better translatables

--- lib/kete_translatable_content/extensions/controllers/translations_controller.rb
+++ lib/kete_translatable_content/extensions/controllers/translations_controller.rb
@@ -9,6 +9,7 @@ TranslationsController.class_eval do
 
   %w{translatable translated}.each do |term|
     define_method("get_" + term) do
+      params[:version] ||= params[@translatable_key][:version] if params[@translatable_key]
       if params[:version]
         value = @translatable_class.find(params[@translatable_key])
         value.revert_to(params[:version].to_i)
@@ -19,26 +20,31 @@ TranslationsController.class_eval do
       instance_variable_set("@" + term, value)
     end
   end
-  # this is the big hammer for translation clearing
-  # it does the entire locale's cache if a translation changes
-  # only use on UI element related models
-  # Kete items (i.e. user generated content) should be done on an item by item basis
+
+  #Expire the cache of documents related to this record works on 
+  #a wider scale if the record is relevant to the global UI.
   def expire_locale_cache
-    # leave Kete items to be cleared individually
-    return true if ZOOM_CLASSES.include?(@translatable_class.to_s)
+    #Kete items to be cleared individually
+    if ZOOM_CLASSES.include?(@translatable_class.to_s)
+      expire_show_caches_for(@translation)
+      return true
+    else
+      # this is the big hammer for translation clearing # it does the entire locale's cache if a translation changes
+      # only use on UI element related models
+      # Kete items (i.e. user generated content) should be done on an item by item basis
+      expire_locale = @translation.locale
+      method_name = "clear_locale_cache"
+      options = { :locale_to_clear => expire_locale }
 
-    expire_locale = @translation.locale
-    method_name = "clear_locale_cache"
-    options = { :locale_to_clear => expire_locale }
-    
-    worker_type = :generic_muted_worker
-    worker_key = worker_key_for("generic_muted_worker_#{method_name}_#{expire_locale}")
+      worker_type = :generic_muted_worker
+      worker_key = worker_key_for("generic_muted_worker_#{method_name}_#{expire_locale}")
 
-    # only allow a single cache clearing to happen at once
-    MiddleMan.new_worker( :worker => worker_type,
-                          :worker_key => worker_key )
+      # only allow a single cache clearing to happen at once
+      MiddleMan.new_worker( :worker => worker_type,
+                           :worker_key => worker_key )
 
-    MiddleMan.worker(worker_type, worker_key).async_do_work( :arg => { :method_name => method_name, :options => options } )
-    true
+      MiddleMan.worker(worker_type, worker_key).async_do_work( :arg => { :method_name => method_name, :options => options } )
+      true
+    end
   end
 end
