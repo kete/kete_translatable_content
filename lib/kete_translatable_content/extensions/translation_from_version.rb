9ea24d95ea4169fa0e8d325da20dfc2b20661207
Breccan <jalnyfen@gmail.com>
2010-12-03 01:51:51 +1300

    Translatable uniqueness of links.

--- lib/kete_translatable_content/extensions/translation_from_version.rb
+++ lib/kete_translatable_content/extensions/translation_from_version.rb
@@ -2,38 +2,53 @@
 # is what has mongo_translatable declaration is on
 module TranslationFromVersion
   unless included_modules.include? TranslationFromVersion
-      #Get the current translation or the latest
-      def translation_for(locale)
-        translation = self.class::Translation.first(self.class.as_foreign_key_sym => id, :locale => locale, :version => self.version.to_s)
-        translation ||= self.class::Translation.first(self.class.as_foreign_key_sym => id, :locale => locale)
-        return translation
+
+
+
+    # sometimes all you need is only the locales of translations because of versioning we need to split out duplicates
+    def translations_locales
+      require 'ruby-debug'
+      debugger
+      translations = self.class::Translation.all(self.class.as_foreign_key_sym => id, :select => 'locale')
+      uniq_translations = []
+      translations.each do |trans|
+        uniq_translations << trans unless uniq_translations.collect(&:locale).include?(trans.locale)
       end
+      uniq_translations
+    end
 
-      def translate(options = {})
-        translation_locale = options[:locale] || I18n.locale
+    #Get the current translation or the latest
+    def translation_for(locale)
+      translation = self.class::Translation.first(self.class.as_foreign_key_sym => id, :locale => locale, :version => self.version.to_s)
+      translation ||= self.class::Translation.first(self.class.as_foreign_key_sym => id, :locale => locale)
+      return translation
+    end
 
-        @translation = self.class::Translation.new({
-          :locale => translation_locale,
-          :translatable_locale => self.locale, # save original locale
-          self.class.as_foreign_key_sym => id,
-          :version => self.version
-        })
+    def translate(options = {})
+      translation_locale = options[:locale] || I18n.locale
 
-        if translation_locale.to_s == original_locale.to_s
-          # TODO: locale's emptiness is the reported error
-          # when this is triggered, figure out why
-          # serving its purpose though to prevent a translation to be added for original_locale
-          @translation.errors.replace(:locale, "Cannot add translation the same as the original locale.")
-        else
-          # work through self and replace attributes
-          # with the passed in translations for defined translatable_attributes
-          translatable_attributes.each do |translated_attribute|
-            translated_value = options[translated_attribute]
-            @translation.send("#{translated_attribute.to_sym}=", translated_value) if translated_value.present?
-          end
+      @translation = self.class::Translation.new({
+        :locale => translation_locale,
+        :translatable_locale => self.locale, # save original locale
+        self.class.as_foreign_key_sym => id,
+        :version => self.version
+      })
+
+      if translation_locale.to_s == original_locale.to_s
+        # TODO: locale's emptiness is the reported error
+        # when this is triggered, figure out why
+        # serving its purpose though to prevent a translation to be added for original_locale
+        @translation.errors.replace(:locale, "Cannot add translation the same as the original locale.")
+      else
+        # work through self and replace attributes
+        # with the passed in translations for defined translatable_attributes
+        translatable_attributes.each do |translated_attribute|
+          translated_value = options[translated_attribute]
+          @translation.send("#{translated_attribute.to_sym}=", translated_value) if translated_value.present?
         end
-
-        @translation
       end
+
+      @translation
     end
   end
+end
